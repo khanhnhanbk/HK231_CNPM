5cdd875fbf6559d7ceb84eff52f74ccad8211faf
khanhnhanbk <nhanhoa21012002@gmail.com>
2023-12-04 15:42:26 +0700

    upload file

--- src/controller/printRequest.controller.js
+++ src/controller/printRequest.controller.js
@@ -1,10 +1,17 @@
-import {samplePrintingRequests} from '../model/samplePrintRequest'
+import { samplePrintingRequests } from '../model/samplePrintRequest'
 
-const updateStatus  = async (id = 0, newStatus) => {
-    console.log('change status')
-    // Update the status in the samplePrintingRequests array
-    samplePrintingRequests[id].Status = newStatus?newStatus:'printed';
-    console.log(samplePrintingRequests[id])
-  };
+const updateStatus = async (id = 0, newStatus) => {
+  console.log('change status')
+  // Update the status in the samplePrintingRequests array
+  samplePrintingRequests[id].Status = newStatus ? newStatus : 'printed';
+  console.log(samplePrintingRequests[id])
+};
 updateStatus();
-export {updateStatus};
+
+const addRequest = async (newRequest) => {
+  console.log('add request')
+  // Update the status in the samplePrintingRequests array
+  samplePrintingRequests.push(newRequest);
+  console.log(samplePrintingRequests)
+}
+export { updateStatus , addRequest};
--- src/pages/extra-pages/UploadPage.js
+++ src/pages/extra-pages/UploadPage.js
@@ -1,20 +1,41 @@
-// material-ui
-import { Typography } from '@mui/material';
-
-// project import
+import { Typography, Button } from '@mui/material';
 import MainCard from 'components/MainCard';
+import { FileUploader } from 'react-drag-drop-files';
+import { addRequest } from '../../controller/printRequest.controller';
+import { useState } from 'react'
+import PrintingRequest from '../../model/printRequest.model'
 
-// ==============================|| SAMPLE PAGE ||============================== //
+const fileTypes = ["doc", "docx", "pdf", "jpg", "jpeg", "png", "txt", "rtf", "xls", "xlsx", "ppt", "pptx", "gif", "bmp", "tif", "tiff", "csv", "zip", "rar", "7z", "gz", "tar", "mp3", "mp4", "mov", "avi", "mkv", "wav", "flac", "ogg", "wma", "aiff", "aac", "psd", "ai", "svg", "eps", "ps", "ttf", "otf", "woff", "woff2", "eot", "xps", "ps", "odt", "ods", "odp", "odg", "odf", "txt", "rtf", "html", "htm", "xhtml", "css", "js", "jsx", "c", "cpp", "h", "hpp", "java", "class", "cs", "vb", "py", "php", "asp", "aspx", "jsp", "xml", "json", "sh", "swift", "go", "rb", "sql", "r", "rmd", "yaml", "yml", "toml", "ini", "cfg", "reg", "conf", "vb", "vbs", "lua", "pl", "ps1", "bat", "cmd", "url", "desktop", "lnk", "md", "markdown", "tex", "log",];
 
-const UploadPage = () => (
-  <MainCard title="Sample Card">
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-      descent molls anim id est labours.  Change this
-    </Typography>
-  </MainCard>
-);
+const UploadPage = () => {
+  const [files, setFiles] = useState(null);
+  const handleChange = (uploadedFiles) => {
+    setFiles(uploadedFiles);
+  };
+  const handleUpload = () => {
+    console.log('Files dropped: ', files);
+    let newRequest = new PrintingRequest(files.name, 'pending', '10/12/2023 10:00 AM', 7);
+    addRequest(newRequest);
+    console.log(files);
+    setFiles(null);
+  };
+  return (
+    <MainCard title="Upload file">
+      <Typography variant="body2">
+        Upload
+      </Typography>
+      <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        sx={{ mt: 3 }}
+        onClick={handleUpload}
+      >
+        Upload
+      </Button>
+    </MainCard>
+  );
+}
 
 export default UploadPage;
